62d8f2eb04a8fe033c4d47dfe15435bd537ef6bc
oraxmonov970-design <oraxmonov970@gmail.com>
2026-07-29 13:34:33 +0500

    Create MainActivity.java

---
+++ app/src/main/java/uz/talimmarkazi/tarixiyshaxslar/MainActivity.java
@@ -0,0 +1,35 @@
+package uz.talimmarkazi.tarixiyshaxslar;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+            }
